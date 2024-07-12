bdebcabda0d341e35c4db071f0946f19a9925edd
apes0 <borislavkosturski@gmail.com>
2024-07-13 00:58:18 +0300

    oops #2 - update the setup script :)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@ git submodule update --init --recursive
 mypath=${0:a:h}
 cd $mypath
 
-sudo apt install -y libxcb-util-dev libx11-xcb-dev libxcb-keysyms1-dev libxcb-image0-dev libxcb-randr0-dev libfontconfig-dev libpango1.0-dev libfreetype6-dev libxcb-xtest0-dev pkg-config python3 build-essential libdbus-glib-1-dev libgirepository1.0-dev xutils-dev xcb-proto
+sudo apt install -y libxcb-util-dev libx11-xcb-dev libxcb-keysyms1-dev libxcb-image0-dev libxcb-randr0-dev libfontconfig-dev libpango1.0-dev libfreetype6-dev libxcb-xtest0-dev pkg-config python3 build-essential libdbus-glib-1-dev libgirepository1.0-dev xutils-dev xcb-proto libtool autoconf
 # add libxcb-ewmh-dev libxcb-icccm4-dev when we support them
 # same for libxcb-xinput-dev
 
